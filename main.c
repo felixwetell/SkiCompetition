0bd033aefac708ef88d106c1afb29b3dccd40016
Felix Wetell <felixwetell@users.noreply.github.com>
2017-12-20 12:38:26 +0100

    Added the main menu, not functioning at the moment

--- main.c
+++ main.c
@@ -12,7 +12,58 @@ struct Skier {
 
 int main () {
 
+    int input;
     int distance = 30000, participants = 42;
+
+    do {
+        printf(":::::::::::::::::::::\n");
+        printf(":::  SKIDTÄVLING  :::\n");
+        printf("1. Nollställ för en ny tävling\n");
+        printf("2. Registera åkare\n");
+        printf("3. Lotta startordning\n");
+        printf("4. Registera åktider\n");
+        printf("5. Visa osorterade resultat\n");
+        printf("6. Visa sorterade resultat\n");
+        printf("0. Avsluta program\n");
+        printf("\n");
+        printf("Välj val: ");
+        scanf("%i", &input);
+        printf("\n");
+
+        switch (input) {
+
+            case 1:
+                
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+
+                break;
+
+            case 0:
+                input = 0;
+                printf("Stänger av program...");
+            default:
+                break;
+        }
+
+    }while (input != 0);
 
     struct Skier participant;
 
@@ -21,21 +72,22 @@ int main () {
     participant.id = 1;
 
     printf("        Förnamn  : ");
-    scanf("%c\n", participant.firstName);
+    scanf("%c\n", &participant.firstName);
 
     printf("        Efternam : ");
-    scanf("%c\n", participant.lastName);
+    scanf("%c\n", &participant.lastName);
 
     printf("        Klubb    : ");
-    scanf("%c\n", participant.club);
+    scanf("%c\n", &participant.club);
 
+    /*
+     FILE *fp;
 
-    FILE *testFile;
-
-    testFile = fopen("test.txt", "r");
-    fprintf(testFile, participant.lastName);
-
-    fclose(testFile);
+    fp = fopen("test.txt", "w+");
+    fprintf(fp, "This is testing for fprintf...\n");
+    fputs("This is testing for fputs...\n", fp);
+    fclose(fp);
+     */
 
     return 0;
 }
